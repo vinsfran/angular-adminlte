ff9761e733504671093c44dab795da0d19ff45f5
Vicente Insfran <vinsfran@gmail.com>
2018-08-15 01:39:44 -0400

    Se agrega Dashboard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import {NgModule} from '@angular/core';
 import {Routes, RouterModule} from '@angular/router';
+import {HomeComponent} from './home/home.component';
+import {PruebaComponent} from './prueba/prueba.component';
+import {Prueba2Component} from './prueba2/prueba2.component';
 
 const routes: Routes = [
+  {
+    path: 'home', component: HomeComponent,
+  },
+  {
+    path: 'prueba', component: HomeComponent,
+    children: [{path: '', component: PruebaComponent}]
+  },
+  {
+    path: 'prueba2', component: HomeComponent,
+    children: [{path: '', component: Prueba2Component}]
+  },
   {path: '', redirectTo: 'login', pathMatch: 'full'}
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,25 @@ import {NgModule} from '@angular/core';
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {LoginModule} from './login/login.module';
+import { AppheaderComponent } from './layouts/appheader/appheader.component';
+import { AppfooterComponent } from './layouts/appfooter/appfooter.component';
+import { AppmenuComponent } from './layouts/appmenu/appmenu.component';
+import { AppsettingsComponent } from './layouts/appsettings/appsettings.component';
+import { HomeComponent } from './home/home.component';
+import { PruebaComponent } from './prueba/prueba.component';
+import { Prueba2Component } from './prueba2/prueba2.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AppheaderComponent,
+    AppfooterComponent,
+    AppmenuComponent,
+    AppsettingsComponent,
+    HomeComponent,
+    PruebaComponent,
+    Prueba2Component
   ],
   imports: [
     BrowserModule,
